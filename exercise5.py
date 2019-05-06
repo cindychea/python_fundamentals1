6e55593efad8970d85a2061db9368c3313ded73d
cindychea <cindy_chea@hotmail.com>
2019-05-06 14:52:01 -0400

    finished fifth exercise base

---
+++ exercise5.py
@@ -0,0 +1,15 @@
+# Create initial while loop
+distance_from_home = 0
+while True:
+    print("Would you like to walk or run?")
+    travel_mode = input()
+    if travel_mode == "walk" or travel_mode == "w":
+        distance_from_home +=1
+    elif travel_mode == "run" or travel_mode == "r":
+        distance_from_home += 5
+    elif travel_mode == "quit" or travel_mode == "q":
+        print("Great job! Goodbye!")
+        break
+    else:
+        print("Invalid answer. Please choose walk, run, or quit.")
+    print("Distance from home is {} km.".format(distance_from_home))
