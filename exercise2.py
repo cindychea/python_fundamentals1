71f1a1ec3b2fc3366cbe9503e31078524734a423
cindychea <cindy_chea@hotmail.com>
2019-05-06 12:30:55 -0400

    finished second exercise

---
+++ exercise2.py
@@ -0,0 +1,13 @@
+# Calculate a good tip for a 55 dollar meal
+# print("How much was your meal?")
+# meal_amount = input()
+# tip_amount = (.20 * int(meal_amount))
+# print("The tip should be {}.".format(tip_amount))
+# total_amount = int(meal_amount) + int(tip_amount)
+# print("The total should be {}.".format(total_amount))
+
+# Result of 45628 multiplied by 7839
+# print("The result of 45628 multiplied by 7839 is {}.".format(45628*7839))
+
+#I think the result will be true
+print((10<20 and 30<20) or not(10==11))
